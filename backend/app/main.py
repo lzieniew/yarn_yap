5f5742daee34f6c06afaa56fb0c7eccc5a7a1319
Łukasz Zieniewicz <lukzieniewicz@gmail.com>
2024-03-18 23:52:27 +0100

    Add list jobs enpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
+from typing import List
 from app.models import Job
 from fastapi import FastAPI
 from pymongo import MongoClient
@@ -17,3 +18,13 @@ async def create_job(job: Job):
     job_dict = job.model_dump()
     result = collection.insert_one(job_dict)
     return {"_id": str(result.inserted_id)}
+
+
+@app.get("/jobs/", response_model=List[Job])
+async def list_jobs():
+    jobs = list(collection.find({}))
+    for job in jobs:
+        job["_id"] = str(
+            job["_id"]
+        )  # Convert ObjectId to string for JSON serializability
+    return jobs
