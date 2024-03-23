ff73e0ac59c63ff6bbda78082fec64be4ff8bbfc
Łukasz Zieniewicz <lukzieniewicz@gmail.com>
2024-03-23 15:08:46 +0100

    Add fetcher and sanitizer

---
+++ worker/fetcher.py
@@ -0,0 +1,4 @@
+def fetch_url(url):
+    content = ""
+    print("Here I will fetch the text from the website and return it")
+    return content
---
+++ worker/sanitizer.py
@@ -0,0 +1,3 @@
+def sanitize(text: str) -> str:
+    print("Here I will sanitize the text")
+    return text
