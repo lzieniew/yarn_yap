4fbcb809c303521acfb84644c64876edd79d1eef
Łukasz Zieniewicz <lukzieniewicz@gmail.com>
2024-04-03 09:23:59 +0200

    TTS with coqui works :D

--- text_to_speech/app/main.py
+++ text_to_speech/app/main.py
@@ -1,22 +1,14 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
-
-
-from scipy.io.wavfile import write
-import text_to_speech.StyleTTS2.ljinference
-import test_to_speech.StyleTTS2.torch
+import torch
+from TTS.api import TTS
 
 
 app = FastAPI()
 
 
 def run_generation():
-    text = "Recent advances in large language models have brought immense value to the world, with their superior capabilities stemming from the massive number of parameters they utilize."
-
-    # text = """ StyleTTS 2 is a text-to-speech model that leverages style diffusion and adversarial training with large speech language models to achieve human-level text-to-speech synthesis. """
-    noise = torch.randn(1, 1, 256).to("cuda" if torch.cuda.is_available() else "cpu")
-    wav = ljinference.inference(text, noise, diffusion_steps=4, embedding_scale=1)
-    write("result.wav", 24000, wav)
+    pass
 
 
 @app.get("/health")
